b00a39845f4b18e6eb26c84b81315d3021b9bc59
guillaume.favre <guillaumefavre56@gmail.com>
2020-07-25 17:52:25 +0200

    Manipulez des listes de composants

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
 import './App.css'
-import GuessCount from './GuessCount'
+
 import Card from './Card'
+import GuessCount from './GuessCount'
+import HallOfFame, {FAKE_HOF} from './HallOfFame'
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 
 class App extends Component {
+  cards = this.generateCards()
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    console.log(result)
+    return shuffle(result)
+  }
 
   handleCardClick(card) {
     console.log(card, 'clicked')
   }
 
   render() {
-    const won = new Date().getSeconds() % 2 === 0;
+    const won = new Date().getSeconds() % 2 === 0
     return (
       <div className="memory">
-        <GuessCount guesses={0}/>
-        <Card card="A" feedback="hidden" onClickCard={this.handleCardClick}/>
-        <Card card="B" feedback="justMatched" onClickCard={this.handleCardClick}/>
-        <Card card="C" feedback="justMismatched" onClickCard={this.handleCardClick}/> 
-        <Card card="D" feedback="visible" onClickCard={this.handleCardClick}/>
-        <Card card="E" feedback="hidden" onClickCard={this.handleCardClick}/>
-        <Card card="F" feedback="justMatched" onClickCard={this.handleCardClick}/>
-        <Card card="G" feedback="hidden" onClickCard={this.handleCardClick}/>
-        {won && <p>Gagné !</p>}
+        <GuessCount guesses={0} />
+        {this.cards.map((card, index) => (
+          <Card card={card} feedback="visible" key={index} onClickCard={this.handleCardClick}/>
+        ))}
+
+        {won && <HallOfFame entries={FAKE_HOF}/>}
+
       </div>
     )
   }
