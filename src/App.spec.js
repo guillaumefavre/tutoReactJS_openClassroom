22a689959caae42b11b1c3b3c5d51ec4d381f3d6
Guillaume FAVRE <guillaumefavre56@gmail.com>
2020-08-10 16:51:52 +0200

    Import librairies test + premiers tests

---
+++ src/App.spec.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { expect } from 'chai'
+import App from './App'
+import GuessCount from './GuessCount'
+
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        // Wrapper autour du composant
+        const wrapper = shallow(<App/>)
+    
+        // On vérifie qu'on a un composant à l'intérieur du wrapper
+        expect(wrapper).to.contain(<GuessCount guesses={0} />)
+    })
+
+    it('has 36 cards', () => {
+        const wrapper = shallow(<App/>)
+
+        expect(wrapper.find('Card')).to.have.length(36)
+    })
+})
